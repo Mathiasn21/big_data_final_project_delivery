34d9e2261a20ee6d9e8afa43725b5f80788ecc16
Mathiasn21 <Mathiasn21@users.noreply.github.com>
2020-10-25 21:07:56 +0100

    Create ProduceMessages.scala

---
+++ src/main/scala/milepæl3/streaming/ProduceMessages.scala
@@ -0,0 +1,34 @@
+package milepæl3.streaming
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+object ProduceMessages {
+  //Credentials(Uname, password, topic)
+
+  private var mapped: Map[String, String] = null
+
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.WARN)
+    Logger.getLogger("akka").setLevel(Level.WARN)
+
+    val spark = SparkSession.builder()
+      .master("local[*]")
+      .appName("Produce messages")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    val streamIn = spark.readStream
+      .format("kafka")
+      .option("kafka.security.protocol", "SASL_SSL")
+      .option("kafka.sasl.mechanism", "SCRAM-SHA-256")
+      .option("kafka.sasl.jaas.config", """org.apache.kafka.common.security.scram.ScramLoginModule required username="aneqi8m2" password="tiYqB_68T6l8OZU30p22LqTrXAsfEmCJ";""")
+      .option("kafka.bootstrap.servers", "rocket-01.srvs.cloudkafka.com:9094,rocket-02.srvs.cloudkafka.com:9094,rocket-03.srvs.cloudkafka.com:9094")
+      .option("subscribe", "aneqi8m2-news")
+      .option("startingOffsets", "earliest")
+      .load()
+  }
+}
