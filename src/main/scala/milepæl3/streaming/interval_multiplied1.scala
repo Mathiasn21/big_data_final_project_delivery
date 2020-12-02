f46ff9668ae2a3a968a978651bfb1a4836f37e9a
marped <marped@users.noreply.github.com>
2020-12-02 16:52:18 +0100

    fixed

--- src/main/scala/milepæl3/streaming/interval_multiplied1.scala
+++ src/main/scala/milepæl3/streaming/interval_multiplied1.scala
@@ -7,7 +7,7 @@ import org.apache.spark.sql.streaming.{OutputMode, Trigger}
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
-object interval_multiplied{
+object interval_multiplied1{
   def main(args:Array[String]):Unit= {
     Logger.getLogger("org").setLevel(Level.ERROR)
     Logger.getLogger("akka").setLevel(Level.ERROR)
@@ -17,6 +17,8 @@ object interval_multiplied{
       .getOrCreate()
 
     import spark.implicits._
+
+    //Reads stream from kafka
     val streamIn = spark.readStream
       .format("kafka")
       .option("kafka.security.protocol", "SASL_SSL")
@@ -27,11 +29,12 @@ object interval_multiplied{
       .option("startingOffsets", "earliest")
       .load()
 
-    //Watermark the stream and cast timestamp to type timestamp
+    //sets watermark with a delay threshold of 1 second
     val waterMarked = streamIn
       .withColumn("timestamp", unix_timestamp(streamIn("timestamp"), "yyyy-MM-dd HH:mm:ss").cast("timestamp"))
       .withWatermark("timestamp", "1 second")
 
+    //
     val formattedDF = waterMarked
       .select($"timestamp", from_json($"value".cast("string"), getSchema).alias("data"))
       .select("timestamp", "data.*")
@@ -39,7 +42,6 @@ object interval_multiplied{
     val trumpFilteredDf = formattedDF
       .filter(lower($"author").contains("trump") || lower($"content").contains("trump"))
 
-    //Group by and count number of occurrences inside the determined time interval
     val myWindow = window($"timestamp", "60 seconds", "60 seconds")
     val trumpWindowed = trumpFilteredDf.groupBy(myWindow).count()
 
@@ -47,31 +49,24 @@ object interval_multiplied{
     var lastTime:Row = null
     val staticWindow = Window.orderBy($"window")
 
-    /*Utilizes a custom function for handling each batch and therein each row
-    Utilized a memory sink as it does not really matter if data is lost in memory in this query.*/
     val query = trumpWindowed.writeStream
       .format("memory")
       .option("truncate", value = false)
       .option("checkpointLocation", "D:\\projects_git\\Semester5\\big_data\\test")
       .foreachBatch { (df: DataFrame, _: Long) => {
-        //Use lag to remember previous values by 1 offset
         val newDf = df.withColumn("prev_count", lag($"count", 1, 0).over(staticWindow))
         newDf.foreach((row:Row) => {
           val count = row.getLong(1)
           val windowStruct = row.getStruct(0)
           var prev = row.getLong(2)
-          //Handle special case where query is first ran or entering a new batch.
           if (lastTime != null && prev == 0 && (windowStruct.equals(lastTime) || windowStruct.getTimestamp(1).equals(lastTime.getTimestamp(0)))){
             prev = lastIntervalCount
           }
-          //Assumed it was greater than or equal, as also accounts for greater rates of trump articles.
-          //Which would make more sense from a business perspective
           if (count >= prev * 2) {
             print("Seeing a doubling of Trump!" + "\n")
             print( "Prev value: " + prev + " Current value: " + count + "\n\n\n")
           }
         })
-        //Reverse sort, such that max value, last entry becomes first entry.
         val maxTime = newDf.sort($"window.end".desc).limit(1)
         val firstVal = maxTime.first()
         lastIntervalCount = firstVal.getLong(1)
